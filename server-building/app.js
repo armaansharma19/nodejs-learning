f2692ba42eafdf13abf3cf48d3dcad0c50f19975
Armaan <armaanpandit@gmail.com>
2025-08-07 10:43:27 +0530

    Create app.js

---
+++ server-building/app.js
@@ -0,0 +1,19 @@
+const http =require('http')
+
+const server=http.createServer((req,res)=>{
+    console.log(req.method,req.headers,req.url);
+    res.setHeader('Content-Type','text/html');//to set the meta data
+    //res.setHeader() is used to set an HTTP response header before sending the body.
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<body>');
+    res.write('<h1>Hello Guys</h1>');
+    res.write('</h1>');
+    res.write('</body>');
+    res.write('</head>');
+    //res.write is used to send chunk of response body to the client
+})
+
+server.listen(3000);
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers
