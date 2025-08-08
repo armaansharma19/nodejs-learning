a3b6fec8d129942f90aee89ab97f2ac40b84ddf1
Armaan <armaanpandit@gmail.com>
2025-08-08 13:46:42 +0530

    update fs

--- server-building/routing.js
+++ server-building/routing.js
@@ -1,8 +1,11 @@
 const http=require('http');
-
+const fs=require('fs');
+const { method } = require('lodash');
 const server=http.createServer((req,res)=>{
     const url =req.url;
+    const method=req.method;
     if(url==='/'){
+        console.log('heeey')
         res.write('<html>');
     res.write('<head>');
     res.write('</head>');
@@ -13,6 +16,25 @@ const server=http.createServer((req,res)=>{
     res.write('</html>');
     return res.end();//ends this block
     // this if block changes the req url to /message and when that req is sent again it dosnt make it to this if block 
+    }
+    if(url==='/message' && method==='POST'){
+        console.log('heeey')
+        const body=[];
+        req.on('data',(e)=>{
+            body.push(e);
+
+        })
+        req.on('end',()=>{
+            const m=Buffer.concat(body).toString();
+            console.log('heeey')
+            const text=m.split('=')[1];
+            fs.writeFileSync('message.txt',text);
+        })
+        
+        //res.writeHead(302);
+        res.statusCode=302;
+        res.setHeader('location','/');
+        return res.end();
     }
     res.write('<html>');
     res.write('<head>');
