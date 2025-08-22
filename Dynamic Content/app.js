121ceae4c8a9356577967976ad2e1d8d592b430f
Armaan <armaanpandit@gmail.com>
2025-08-22 09:32:44 +0530

    using pug

--- Dynamic Content/app.js
+++ Dynamic Content/app.js
@@ -1,15 +1,21 @@
-const express=require('express');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const adminData=require('./routes/admin.js')
 const shopRoute=require('./routes/shop.js')
 const app=express();
 
+
 app.set('view engine','pug');
-app.set('views','views')//to set the templating engine folder (it is set to /views by default)
+//app.set('views','views')//to set the templating engine folder (it is set to /views by default)
+
 
 app.use(adminData.routes);
 app.use(shopRoute);
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000,()=>{
     console.log("Server running on 3000");
--- Dynamic Content/routes/shop.js
+++ Dynamic Content/routes/shop.js
@@ -5,8 +5,9 @@ const adminData=require('./admin');
 const route=express.Router();
 
 route.get('/',(req,res,next)=>{
-    console.log('shop.js',adminData.products);
-    res.sendFile(path.join(__dirname, '../views', 'shop.html'));
+   // console.log('shop.js',adminData.products);
+   // res.sendFile(path.join(__dirname, '../views', 'shop.html'));
+   res.render('shop')
 })
 
 module.exports=route;
